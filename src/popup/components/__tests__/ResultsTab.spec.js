fa999f6774fc35b7fd87558aadcf9107288f143e
oscartavarez <tavarez.oscar>
2019-10-08 17:45:52 -0400

    adjusting version in ui

--- src/popup/components/__tests__/ResultsTab.spec.js
+++ src/popup/components/__tests__/ResultsTab.spec.js
@@ -1,23 +1,21 @@
-import { createLocalVue, mount } from '@vue/test-utils'
-import ResultsTab from '../ResultsTab'
+import { createLocalVue, mount } from "@vue/test-utils";
+import ResultsTab from "../ResultsTab";
 
-describe('RecordingTab.vue', () => {
-  let localVue
+describe("RecordingTab.vue", () => {
+  let localVue;
   beforeEach(() => {
-    localVue = createLocalVue()
-    localVue.directive('highlightjs', () => {})
-  })
+    localVue = createLocalVue();
+    localVue.directive("highlightjs", () => {});
+  });
 
-  test('it has the correct pristine / empty state', () => {
-    const wrapper = mount(ResultsTab, { localVue })
-    expect(wrapper.element).toMatchSnapshot()
-    expect(wrapper.find('code.javascript').exists()).toBe(false)
-  })
+  test("it has the correct pristine / empty state", () => {
+    const wrapper = mount(ResultsTab, { localVue });
+    expect(wrapper.find("code.javascript").exists()).toBe(false);
+  });
 
-  test('it show a code box when there is code', () => {
-    const wrapper = mount(ResultsTab, { localVue })
-    wrapper.setProps({ code: `cy.get('.class').clic()` })
-    expect(wrapper.element).toMatchSnapshot()
-    expect(wrapper.find('code.javascript').exists()).toBe(true)
-  })
-})
+  test("it show a code box when there is code", () => {
+    const wrapper = mount(ResultsTab, { localVue });
+    wrapper.setProps({ code: `cy.get('.class').clic()` });
+    expect(wrapper.find("code.javascript").exists()).toBe(true);
+  });
+});
